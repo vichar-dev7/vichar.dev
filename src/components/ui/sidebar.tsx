165035697eb2cbc4f807c33706276b417b851a91
omkar861856 <omkar861856@gmail.com>
2024-10-04 18:41:21 +0530

    dash broilerplate

---
+++ src/components/ui/sidebar.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React from 'react'
+import { Nav } from './nav'
+import {
+    ChevronRight,
+    ChevronLeft,
+    LayoutDashboard,
+    Settings,
+    ShoppingCart,
+    UsersRound,
+  } from "lucide-react"
+import { Button } from './DashButton'
+
+import { useWindowWidth } from '@react-hook/window-size'
+
+type Props = {}
+
+export default function Sidebar({}: Props) {
+
+    const [isCollapsed, setIsCollapsed] = React.useState(false)
+
+    const onlyWidth = useWindowWidth()
+    const mobileWidth = onlyWidth < 768
+
+    function toggleSidebar(){
+        setIsCollapsed(!isCollapsed)
+    }
+    return (
+        <div className='relative min-w-[80px] border-r px-3 pb-10 pt-24'>
+            {!mobileWidth &&
+                <div className='absolute right-[-20px] top-7'>
+                    <Button variant='secondary' className='rounded-full p-2' onClick={toggleSidebar}>
+                        {isCollapsed?<ChevronRight/>:<ChevronLeft />}
+                        
+                    </Button>
+                </div>
+            }
+            <Nav
+                isCollapsed={mobileWidth ? true : isCollapsed}
+                links={[
+                    {
+                    title: "Dashboard",
+                    href: "/dashboard",
+                    icon: LayoutDashboard,
+                    variant: "default",
+                    },
+                    {
+                    title: "Hackathon Updates",
+                    href: "/hackathon",
+                    icon: UsersRound,
+                    variant: "ghost",
+                    },
+                    {
+                    title: "Join Community",
+                    href: "/community",
+                    icon: ShoppingCart,
+                    variant: "ghost",
+                    },
+                    {
+                    title: "Post Jobs",
+                    href: "/jobs",
+                    icon: Settings,
+                    variant: "ghost",
+                    },
+                ]}
+            />
+        </div>
+    )
+}
